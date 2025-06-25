2fa22915dabda4fba746cdcd097c82d8606666b8
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-06-25 13:45:44 +1000

    movies list

---
+++ angular-movies/src/app/movies/movies-list/movies-list.component.ts
@@ -0,0 +1,36 @@
+import {
+  CurrencyPipe,
+  DatePipe,
+  NgFor,
+  NgIf,
+  NgOptimizedImage,
+  UpperCasePipe,
+} from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-list',
+  standalone: true,
+  imports: [DatePipe, CurrencyPipe, UpperCasePipe, NgIf, NgFor],
+  templateUrl: './movies-list.component.html',
+  styleUrl: './movies-list.component.scss',
+})
+export class MoviesListComponent {
+  @Input({ required: true }) movies!: any[];
+
+  addMovie(): void {
+    this.movies.push({
+      title: 'Inception',
+      releaseDate: new Date('2012-05-03'),
+      price: 500.99,
+      poster: null,
+    });
+  }
+
+  removeMovie(movie: any) {
+    const indexToRemove = this.movies.findIndex(
+      (x: any) => x.Title === movie.Title
+    );
+    this.movies.splice(indexToRemove, 1);
+  }
+}
