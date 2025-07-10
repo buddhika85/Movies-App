457434de7cfc1dc6876bb605152d0b4541d997bb
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-07-10 11:39:25 +1000

    congirations added

--- angular-movies/src/app/shared/services/genres.service.ts
+++ angular-movies/src/app/shared/services/genres.service.ts
@@ -2,14 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { GenreDto } from '../models/genre.models';
 import { Observable } from 'rxjs';
+import { environment } from './../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GenresService {
   private httpClient: HttpClient = inject(HttpClient);
+  private baseUrl: string = `${environment.apiURL}/Genres`;
 
   public getAll(): Observable<GenreDto[]> {
-    return this.httpClient.get<GenreDto[]>('https://localhost:7045/api/Genres');
+    return this.httpClient.get<GenreDto[]>(this.baseUrl);
   }
 }
